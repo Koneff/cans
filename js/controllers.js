23f273c03cc6a88eafef55b973d4648727f75004
Koneff <a.konefff@gmail.com>
2015-05-29 01:55:49 +0300

    Added loading products-info Fixed modal windows

--- js/controllers.js
+++ js/controllers.js
@@ -7,21 +7,12 @@ angular.module('cans.controllers',['cans.services'])
         };
     })
 
-.controller('ModalWindowCtrl',function($scope,$modal){
-        $scope.showModalWindow = true;
-        $scope.toggleModalWindow = function(){
-            $scope.showModalWindow = $scope.showModalWindow === false ? true: false;
-            console.log($scope.showModalWindow)
-        }
-
-    })
 
 .controller('CarouselCtrl',function($scope){
         $scope.slideInterval = 7000;
         var slides = $scope.slides = [];
         $scope.addSlide = function(){
             var newQuantity = slides.length + 1;
-            console.log(newQuantity);
             slides.push({
                 image: 'img/' + newQuantity + '.jpg',
                 text: ['Скумбрия в томатном соку','Толстолобик маринованный','Сельдь с лимоном'][slides.length%3]
@@ -47,23 +38,10 @@ angular.module('cans.controllers',['cans.services'])
         })
 
 .controller('ProductCtrl',function($scope,$http,Products){
-        $scope.submitLoading = function(){Products.getProducts()
+            Products.getProducts()
             .then(function(){
                 $scope.allProducts = Products.queue;
                 console.log($scope.allProducts)
             });
-        }
 
-        /*$scope.dataLoaded = false;
-
-        $scope.submitLoading = function(){
-            Products.getProducts().then(function(successResponse){
-                $scope.dataLoaded = true;
-                $scope.productInfo = Products.queue;
-                console.log($scope.productInfo);
-            },function(failureResponse){
-                $scope.dataLoaded = false;
-                console.log(failureResponse);
-            })
-        }*/
     });
--- js/services.js
+++ js/services.js
@@ -82,4 +82,4 @@ angular.module('cans.services',[])
         };
         return o;
 
-    })
+    });
