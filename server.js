1bf7b3caf44cec15968ef99260d8bae8eb695d28
Koneff <a.konefff@gmail.com>
2015-06-06 22:45:17 +0300

    Node.js server prototype added

--- server.js
+++ server.js
@@ -11,28 +11,9 @@ var mandrill = require('mandrill-api/mandrill');
 
 mandrill_client = new mandrill.Mandrill(API_KEY);
 
-var message = {
-    "text": "Example text content",
-    "subject": "example subject",
-    "from_email": "message.from_email@example.com",
-    "from_name": "Example Name",
-    "to": [{
-        "email": "prosoldp@gmail.com",
-        "name": "Artem",
-        "type": "to"
-    }],
-    "headers": {
-        "Reply-To": "message.reply@example.com"
-    }
-};
-var async = false;
-var ip_pool = "Main Pool";
-//var send_at = "example send_at";
-mandrill_client.messages.send({"message": message, "async": async, "ip_pool": ip_pool}, function(result) {
-    console.log(result);
+var app = express();
+app.use(bodyParser.json());
 
-}, function(e) {
-    // Mandrill returns the error as an object with name and message keys
-    console.log('A mandrill error occurred: ' + e.name + ' - ' + e.message);
-    // A mandrill error occurred: Unknown_Subaccount - No subaccount exists with the id 'customer-123'
-});
+
+var apiPort = process.env['PORT']||3000;
+app.listen(apiPort);
